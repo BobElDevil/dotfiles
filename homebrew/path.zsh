9872480ea3bafa3229e90d888593eed9f1db4d0f
Stephen Marquis <marquissm@gmail.com>
2023-11-29 12:54:10 -0800

    Some path updates

---
+++ homebrew/path.zsh
@@ -0,0 +1,2 @@
+export DYLD_LIBRARY_PATH="/opt/homebrow/lib:$DYLD_LIBRARY_PATH";
+eval $(/opt/homebrew/bin/brew shellenv zsh)
--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,1 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$ZSH/local-dotfiles/bin:$PATH"
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export PATH="$ZSH/bin:$ZSH/local-dotfiles/bin:$PATH"
