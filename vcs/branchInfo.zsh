d8996ea8e9d26d9a5fdd06fea9bfaccf41feaf52
Stephen Marquis <steve.marquis@grammarly.com>
2025-07-24 12:34:25 -0700

    Compute on demand without invalid check

--- vcs/branchInfo.zsh
+++ vcs/branchInfo.zsh
@@ -12,18 +12,13 @@ function vcs_compute_location() {
   local GIT_BRANCH="$(parse_git_branch)"
   local JJ_CHANGE="$(parse_jj_changeId)"
 
-  export __CURRENT_VCS_LOCATION_INVALID=
   if [ -n "$JJ_CHANGE" ]; then
-    export __CURRENT_VCS_LOCATION=$JJ_CHANGE
+    export __CURRENT_VCS_LOCATION="$JJ_CHANGE"
   elif [ -n "$GIT_BRANCH" ]; then
-    export __CURRENT_VCS_LOCATION=$GIT_BRANCH
+    export __CURRENT_VCS_LOCATION="$GIT_BRANCH"
   else
     export __CURRENT_VCS_LOCATION=""
   fi
-}
-
-function vcs_invalidate_location() {
-	export __CURRENT_VCS_LOCATION_INVALID=1
 }
 
 typeset -ga chpwd_functions
@@ -32,19 +27,18 @@ typeset -ga preexec_functions
 preexec_functions+='preexec_update_vcs_location'
 preexec_update_vcs_location() {
 	case $2 in 
-    git*) vcs_invalidate_location ;;
-    jj*) vcs_invalidate_location ;;
+    git*) vcs_compute_location ;;
+    jj*) vcs_compute_location ;;
 	esac
 }
 
 chpwd_functions+='chpwd_update_vcs_location'
 chpwd_update_vcs_location() {
-	vcs_invalidate_location
+  vcs_compute_location
 }
 
 function vcs_get_location() {
-	test -n "$__CURRENT_VCS_LOCATION_INVALID" && vcs_compute_location
-	echo $__CURRENT_VCS_LOCATION
+  echo $__CURRENT_VCS_LOCATION
 }
 
 vcs_compute_location
